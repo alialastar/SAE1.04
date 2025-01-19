6b3a25ee464d7a4f377db1949034967163311817
alialastar <alia.abdedaime@etu.univ-st-etienne.fr>
2025-01-19 15:47:26 +0100

    projet

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,26 +7,33 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 { 
+    #[Route('/blog/portefolio', name: 'app_portefolio')]
+    public function portefolio(): Response 
+    { 
+        return $this->render('blog/portefolio.html.twig',[
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/blog/loisir', name: 'app_loisir')]
+    public function loisir(): Response 
+    { 
+        return $this->render('blog/loisir.html.twig',[
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/blog/cv', name: 'app_cv')]
+    public function cv(): Response 
+    { 
+        return $this->render('blog/cv.html.twig',[
+            'controller_name' => 'BlogController',
+        ]);
+    }
     #[Route('/blog/accueil', name: 'app_accueil')]
     public function accueil(): Response 
     { 
         return $this->render('blog/accueil.html.twig',[
-            'controller_name' => 'BlogController',
-        ]);
-    }
-
-    #[Route('/blog', name: 'app_blog')]
-    public function blog(): Response 
-    { 
-        return $this->render('blog/blog.html.twig',[
-            'controller_name' => 'BlogController',
-        ]);
-    }
-
-    #[Route('/blog/new', name: 'app_new')]
-    public function new(): Response 
-    { 
-        return $this->render('blog/new.html.twig',[
             'controller_name' => 'BlogController',
         ]);
     }
